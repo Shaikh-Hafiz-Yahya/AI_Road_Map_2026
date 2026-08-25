35f08f2d8fb075f8e32a3001cdb137634d7b134a
Muhammad Yahya <hafizmuhammadyahya2003@gmail.com>
2026-08-25 14:37:16 +0500

    Module03 FastApi

--- Module03_FastAPI/main.py
+++ Module03_FastAPI/main.py
@@ -7,6 +7,40 @@
 # uvicorn --> 'server ha fastapi ka'
 # pydantic(pyd) --> is a 'translator'
 # ------------------------------------------------------------
+# {2:4} --> Dictionary
+# {'2':4} --> JSON ()
+# ------------------------------------------------------------
+# 2. JSON kya hai?
+# JSON = JavaScript Object Notation
+# JSON ek data exchange format hai. Iska purpose different applications/systems ke darmiyan data transfer karna hai.
+# JSON:
+# {
+#     "name": "Aatif",
+#     "age": 25,
+#     "doctor": true
+# }
+# ------------------------------------------------------------
+# Ye Python dictionary jaisa dikh raha hai, lekin technically ye JSON data hai.
+# Notice:
+# Python:
+# "doctor": True
+# "doctor": True
+
+# JSON:
+# "doctor": true
+# ------------------------------------------------------------
+# Python mein:
+# True
+# False
+# None
+
+# JSON mein:
+# true
+# false
+# null
+# ------------------------------------------------------------
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
